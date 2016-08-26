bea93e640695446519eb1d2311bb6cd75ff560a4
Connor Murray <connormurray7@gmail.com>
2016-08-25 22:58:56 -0500

    Comments for dynamic array

--- python/data_structures/dynamic_array.py
+++ python/data_structures/dynamic_array.py
@@ -1,9 +1,20 @@
-"""Simple implementation of a dynamically resized array with Ring Buffer.
+"""
+Simple implementation of a dynamically resized array with Ring Buffer.
 
+Connor Murray (connormurray7@gmail.com)
+on 8/25/2016
 """
 
 class Array:
-    """Implemented with a statically sized array.
+    """Implemented with a python list because there are no static arrays.
+
+    Uses a ring buffer to get objects.
+
+    Attributes:
+        INITIAL_SIZE: integer, "size" of the static array
+        index: current element that the ring buffer is on, wraps at buffer size
+        array: python list that holds the data
+        buffer: current maximum size of the array
 
     """
     INITIAL_SIZE = 16
@@ -14,23 +25,27 @@ class Array:
         self.buffer = size
         
     def __getitem__(self, index):
+        """Overloads operator [] for random access if exists."""
         if index >= 0 and index < self.buffer:
             return self.array[index]
         else:
             return None
         
     def append(self, item):
+        """If size is the same as the buffer, array doubles in size."""
         if len(self.array) == self.buffer:
             self.buffer *= 2
 
         self.array.append(item)
             
     def get(self):
+        """Returns what element the ring buffer is currently on.""" 
         item = self.array[self.index]
         self.index = (self.index + 1) % (self.buffer)
         return item
 
     def remove(self,index):
+        """Removes element at index, if it exists."""
         if index < 0 or index >= self.buffer:
             return
         self.array.pop(index)
