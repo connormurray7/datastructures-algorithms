8914e65f112a88e6fbc632c726ea2e2903814e93
Connor Murray <connormurray7@gmail.com>
2016-08-27 16:25:13 -0500

    Fixed bug in add definition

--- python/data_structures/hash_set.py
+++ python/data_structures/hash_set.py
@@ -16,24 +16,37 @@ class HashSet:
 
     """
 
-    def __init__(self, init_size=16, hash=None, load_factor=0.75):
+    def __init__(self, init_size=16, h=None, load_factor=0.75):
         self.load_factor = load_factor
+        self.size = 0
         self.max_size = init_size
-        self.buckets = [None]*max
-        if self.hash is None:
-            self.hash = lambda x: x % self.max
+        self.buckets = [None]*init_size
+        if h is None:
+            self.hash = lambda x: x % self.max_size
         else:
-            self.hash = hash
+            self.hash = h
+
+    def __str__(self):
+        keys = []
+        for b in self.buckets:
+            if b is not None:
+                keys.append(b)
+        out_str = ' '.join([str(k) for k in keys])
+        return "Hashset [" + out_str + "]"
+            
 
     def grow(self):
         self.max_size *= 2
         tmp = self.buckets
-        self.buckets = [None]*self.max
+        self.buckets = [None]*self.max_size
 
         for k in tmp:
             self.add(k)
 
     def add(self,key):
-        if len(self.buckets)/self.max_size > load_factor:
-            grow()
-        self.buckets[self.hash(key)] = key
+        if self.size/self.max_size > self.load_factor:
+            self.grow()
+        b = self.hash(key)
+        if self.buckets[b] is None:
+            self.size += 1
+        self.buckets[b] = key
