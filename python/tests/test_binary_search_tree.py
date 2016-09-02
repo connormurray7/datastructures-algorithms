5dc299fa128bff0ca6dcb8295cf556324adbd59f
Connor Murray <connormurray7@gmail.com>
2016-09-01 23:20:31 -0500

    Testing binary seearch tree

---
+++ python/tests/test_binary_search_tree.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import unittest
+sys.path.append(os.path.join(os.getcwd(), os.path.pardir))
+
+from data_structures.binary_search_tree import BinarySearchTree
+
+class HashMapTest(unittest.TestCase):
+
+    def test_get(self):
+        print("Testing add")
+        b = BinarySearchTree()
+
+        b.put(1, "foo")
+        b.put(2, "bar")
+        b.put(3, "baz")
+
+        self.assertEqual(b.get(1), "foo")
+        self.assertEqual(b.get(2), "bar")
+        self.assertEqual(b.get(3), "baz")
+        self.assertEqual(b.get(0), None)
+
+if __name__ == '__main__':
+    unittest.main()
