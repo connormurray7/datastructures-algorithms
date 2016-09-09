6eaa42de5846fafd209316a9df819f8b5cf3b93d
Connor Murray <connormurray7@gmail.com>
2016-09-08 21:01:21 -0500

    started object oriented graph

---
+++ python/data_structures/graph.py
@@ -0,0 +1,23 @@
+"""
+Created by Connor Murray (connormurray7@gmail.com)
+8/27/2016
+
+Python implementation of a graph using an adjacency list.
+"""
+
+class Graph:
+    """Python implementation of a graph using an adjacency list.
+
+    Attributes:
+
+    """
+    class Vertex:
+        """Vertex in a graph."""
+        def __init__(self, key):
+            self.key = key
+
+    class Edge:
+        """Connect two vertices."""
+        def __init__(self, v1, v2, w):
+            self.
+
