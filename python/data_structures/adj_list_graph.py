92bb76176a394afe8982b7f4afc446640b92dc7f
Connor Murray <connormurray7@gmail.com>
2016-09-08 22:31:23 -0500

    Graph abs class and adj list graph

---
+++ python/data_structures/adj_list_graph.py
@@ -0,0 +1,36 @@
+"""
+Created by Connor Murray (connormurray7@gmail.com)
+8/27/2016
+
+Python implementation of a graph using an adjacency list.
+"""
+from graph import Graph
+
+class AdjListGraph(Graph):
+    """Python implementation of a graph with adj list.
+
+    Attributes:
+        init: constructor
+        add_vertex: constructs a vertex and keeps track of it
+    """
+
+    def __init__(self):
+        print("it worked wooo!")
+        self.adj_list = {}
+
+    def add_vertex(self, key):
+        self.adj_list[key] = []
+
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list:
+            self.adj_list[v1].append(v2)
+        if v2 in self.adj_list:
+            self.adj_list[v2].append(v1)
+
+    def __str__(self):
+        """String representation of set"""
+        vertices = []
+        for vert in self.adj_list:
+            vertices.append(vert)
+        out_str = ' '.join([str(v) for v in vertices])
+        return "Graph [" + out_str + "]"
--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -2,22 +2,36 @@
 Created by Connor Murray (connormurray7@gmail.com)
 8/27/2016
 
-Python implementation of a graph using an adjacency list.
+Python abstract class of a graph.
 """
+from abc import ABCMeta
+
+class Vertex:
+    """Vertex in a graph."""
+    def __init__(self, key):
+        self.key = key
+
+class Edge:
+    """Connects two vertices with optional weight."""
+
+    def __init__(self, v1, v2, w=None):
+        self.edge = (v1, v2)
+        self.weight = w
 
 class Graph:
-    """Python implementation of a graph using an adjacency list.
+    """Python abstract class of a graph.
 
     Attributes:
-
+        init: constructor
+        add_vertex: constructs a vertex and keeps track of it
     """
-    class Vertex:
-        """Vertex in a graph."""
-        def __init__(self, key):
-            self.key = key
+    __metaclass__ = ABCMeta
 
-    class Edge:
-        """Connect two vertices."""
-        def __init__(self, v1, v2, w):
-            self.
+    def __init__(self):
+        pass
 
+    def add_vertex(self, key):
+        pass
+
+    def __str__(self):
+        pass
