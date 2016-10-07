e8f735de3caaa590ff21d81942668a01111f7b19
Connor Murray <connormurray7@gmail.com>
2016-10-06 20:57:58 -0500

    fixed lcs bug

--- python/algorithms/longest_common_subsequence.py
+++ python/algorithms/longest_common_subsequence.py
@@ -7,13 +7,16 @@ Python implementation of Longest Common Subsequence
 
 def lcs(word1, word2):
 
-    matrix = [[0]*len(word2)]*len(word1)
+    m = len(word1) + 1
+    n = len(word2) + 1
+
+    matrix = [[0] * n for i in range(m)]
     
-    for i in range(1, len(word1)):
-        for j in range(1, len(word2)):
-            if word1[i] == word2[j]:
+    for i in range(1, m):
+        for j in range(1, n):
+            if word1[i-1] == word2[j-1]:
                 matrix[i][j] = matrix[i-1][j-1] + 1
             else:
                 matrix[i][j] = max(matrix[i][j-1], matrix[i-1][j])
 
-    return matrix[len(word1)-1][len(word2)-1]
+    return matrix[m-1][n-1]
