e42f7f0cede50e29499711e48f4c31dc5ac755bf
Connor Murray <connormurray7@gmail.com>
2016-12-04 15:43:28 -0600

    starting implementation

---
+++ cpp/data_structures/vector/Vector.cpp
@@ -0,0 +1,34 @@
+Vector(){}
+
+Vector(Vector<T> &vector_in){}
+    
+~Vector(){}
+
+
+T operator[](int idx){}
+    
+T at(int idx){}
+
+T* front(){}
+
+T* back(){}
+
+bool empty(){}
+
+unsigned int size(){}
+
+void push_back(T val){}
+
+void pop_back(){}
+    
+void resize(int num){}
+
+T[] arr{}
+
+int capacity = 1{}
+
+int num_elements = 0{}
+
+void grow(){}
+
+bool full(){}
