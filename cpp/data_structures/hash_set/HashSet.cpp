854cfc7673a4ec0de6d29dbbdf3cf975c5084aaf
Connor Murray <connormurray7@gmail.com>
2016-12-17 12:18:13 -0600

    hash set impl

---
+++ cpp/data_structures/hash_set/HashSet.cpp
@@ -0,0 +1,41 @@
+#include "HashSet.cpp"
+
+template<typename T, typename H>
+HashSet<T,H>::HashSet() {
+
+}
+
+template<typename T, typename H>
+HashSet<T,H>::HashSet(const HashSet& other) {
+
+}
+
+template<typename T, typename H>
+HashSet<T,H>::~HashSet() {
+
+}
+
+template<typename T, typename H>
+void HashSet<T,H>::insert(const T& key) {
+
+}
+
+template<typename T, typename H>
+void HashSet<T,H>::erase(const T& key) {
+
+}
+
+template<typename T, typename H>
+bool HashSet<T,H>::contains(const T& key) {
+
+}
+
+template<typename T, typename H>
+unsigned int HashSet<T,H>::size() {
+
+}
+
+template<typename T, typename H>
+bool HashSet<T,H>::empty() {
+    
+}
