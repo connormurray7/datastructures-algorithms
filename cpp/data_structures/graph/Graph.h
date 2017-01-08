a93ee9ad2cebd448d894fc10b0f76765fa5e4e2c
Connor Murray <connormurray7@gmail.com>
2017-01-07 22:08:43 -0600

    graph

--- cpp/data_structures/graph/Graph.h
+++ cpp/data_structures/graph/Graph.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <memory>
 
 #ifdef GRAPH_H
 #define GRAPH_H
@@ -6,21 +7,54 @@
 template<typename T>
 struct GraphNode {
 
-    GraphNode(const T& val) : val(val) {}
+    GraphNode(const T& val) {
+        val = val_in;
+        visited = false;
+    }
 
     T val;
-    std::vector<GraphNode<T>> edges;
+    bool visited;
+    std::vector<int> edges;
 };
 
 template<typename T>
-struct Graph {
-    
-    Graph() {
-        nodes = std::vector<GraphNode<T>>();
-    }
+class Graph {
+public:
 
-    std::vector<GraphNode<T>> nodes;
+    Graph();
+
+    Graph(const Graph& other);
+
+    ~Graph();
+
+    int add_node(const T& val);
+
+    void add_edge(int node1, node2);
+
+    int size();
+
+    std::vector<GraphNode<T>*> nodes;
 };
+
+template<typename T>
+Graph<T>::Graph() {
+    nodes = std::vector<GraphNode<T>>();
+}
+
+template<typename T>
+Graph<T>::Graph(const Graph<T>& other) {
+    nodes = other.nodes;
+}
+
+template<typename T>
+Graph<T>::~Graph() {}
+
+template<typename T>
+int Graph<T>::add_node(const T& val) {
+    int num = nodes.size();
+    nodes.push_back(std::make_shared(val));
+}
+
 
 
 
